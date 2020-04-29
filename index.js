e15207c3b841cae216a373779fc78c326beeda1b
shivraj-ib <shivraj.singh@infobeans.com>
2020-04-29 17:39:38 +0530

    node_setup

--- index.js
+++ index.js
@@ -3,19 +3,31 @@
 /**
  * Required External Modules
  */
+const express = require("express");
+const path = require("path");
 
 /**
  * App Variables
  */
+const app = express();
+const port = process.env.PORT || "9000";
 
 /**
  *  App Configuration
  */
+app.set("views", path.join(__dirname, "./src/views"));
+app.set("view engine", "pug");
 
 /**
  * Routes Definitions
  */
+app.get("/", (req, res) => {
+    res.render("index", { title: "Home" });
+});
 
 /**
  * Server Activation
  */
+app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+});
